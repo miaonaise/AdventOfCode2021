28c6d802de35c96ae9b5939f51cd17e6ef53facd
Julia Li <julia.li@lynxhedge.se>
2021-12-22 19:47:03 +0100

    Add project files.

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        public static List<int> depths = File.ReadAllLines(@"D:\src\myProject\AdventOfCode\AdventOfCode\Day1.txt").Select(int.Parse).ToList();
+
+        public static int Part1()
+        {
+            int count = 0;
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 1])
+                {
+                    count++;
+                }
+                
+            }
+            return count;
+        }
+
+        public static int Part2()
+        {
+            int count = 0;
+            for (int i = 3; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 3])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.Part2());
+        }
+    }
+}
